3d79a1a5928eb0bfcb11b8df571f0ef0a98e19b9
iRishabhSingh <rishab18011@gmail.com>
2024-04-13 15:52:41 +0530

    Enhanced Header Component and Implemented it in Page.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,39 @@ import React from "react";
 import { Text } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "./Sidebar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 const Header = () => {
   return (
-    <div className="md:hidden">
-      <Sheet>
-        <SheetTrigger>
-          <Text width={20} height={20} />
-        </SheetTrigger>
-        <SheetContent side={"left"} className="w-[260px] p-0">
-          <Sidebar />
-        </SheetContent>
-      </Sheet>
-    </div>
+    <header className="sticky flex items-center justify-between dark:bg-[#131110]">
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Text width={20} height={20} />
+          </SheetTrigger>
+          <SheetContent side={"left"} className="w-[260px] p-0">
+            <Sidebar />
+          </SheetContent>
+        </Sheet>
+      </div>
+      <Select>
+        <SelectTrigger className="w-[180px] h-10">
+          <SelectValue placeholder="ChatVirtuoso 3.5" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="gpt-3.5-turbo">ChatVirtuoso 3.5</SelectItem>
+          <SelectItem value="gpt-4" disabled>
+            ChatVirtuoso 4
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </header>
   );
 };
 
